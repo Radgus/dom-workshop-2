d091b7469fab791c74bbfc84aa7fb8e720328120
Radgus <dominguezradgus@gmail.com>
2022-01-30 08:36:28 -0600

    workshop finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import h from 'hyperscript';
+import { registerImage } from "./lazy"; 
 
-console.log('Happy hacking :)')
+
+const maxinum = 123;
+const mininum = 1;
+const random = () => Math.floor(Math.random() * (maxinum - mininum)) + mininum
+
+const createImageNode = () => {
+  // const container = document.createElement('div');
+  // container.className = 'p-4';
+  // const container = h('div.p-4');
+
+  // const imagen = document.createElement('img');
+  // imagen.className = "mx-auto";
+  // imagen.width = '320';
+  // imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+  const imagen = h('img.mx-auto',{
+    width: '320',
+    'data-src': `https://randomfox.ca/images/${random()}.jpg`,
+  });
+
+  const container = h('div.p-4', imagen);
+  // const container = <div className='p-4'><imagen /></div>  // h -> html (React)
+
+  // container.appendChild(imagen);
+
+  return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('image');
+
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.appendChild(newImage);
+  registerImage(newImage);
+};
+
+addButton.addEventListener('click', addImage)
+
+
+
+
